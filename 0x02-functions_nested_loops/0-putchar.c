9c88984454d07aa6b25b7265a48ef3894c76727e
mwendwa-will <williammwendwa@gmail.com>
2023-02-21 11:36:20 +0300

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point of the program
+ *   *
+ *    * Description: Prints the string "_putchar"
+ *    followed by a newline character
+ *     *
+ *      * Return: Always 0 (success)
+ **/
+int main(void)
+{
+	/* Use the printf function to print the string "_putchar" to the console */
+	printf("_putchar\n");
+
+	/* Return 0 to indicate that the program terminated successfully */
+	return (0);
+}
+
