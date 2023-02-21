a5db5c9ff391208d594a8f6ddb685394c56dbc0e
mwendwa-will <williammwendwa@gmail.com>
2023-02-21 12:18:48 +0300

    1.alphabet.c

---
+++ 0x02-functions_nested_loops/1.alphabet.c
@@ -0,0 +1,19 @@
+
+
+/**
+ ** print_alphabet - Prints the alphabet in lowercase using only putchar
+ *
+ **/
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+
+}
