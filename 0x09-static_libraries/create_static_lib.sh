54c14aab9536b4c54238fd0d78415f9baa7e354e
mwendwa-will <williammwendwa@gmail.com>
2023-03-15 13:30:17 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# Compile each .c file into an object file
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-
-# Create the static library from the object files
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc liball.a *.o
-
-# Index the library for faster symbol lookup
 ranlib liball.a
-
-# Clean up the object files
-rm *.o
-
